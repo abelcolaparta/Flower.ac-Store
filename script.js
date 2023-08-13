3e8245c60862b9c1526c2339497ecdfe7a9743b4
abelcolaparta <laparta2411@gmail.com>
2023-08-13 16:33:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const navBar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  const windowPosition = window.scrollY > 1;
+  navBar.classList.toggle("scrolling-active", windowPosition);
+});
